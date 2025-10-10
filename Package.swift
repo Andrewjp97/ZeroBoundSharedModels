7611a8fceea6f036fa5ae9bf09b73a2762e5577e
Andrew Paterson <Andrewjp97@me.com>
2025-10-10 17:57:54 -0400

    switching to openapi

--- Package.swift
+++ Package.swift
@@ -5,16 +5,43 @@ import PackageDescription
 
 let package = Package(
     name: "ZeroBoundSharedModels",
+    platforms: [
+        .macOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "ZeroBoundSharedModels",
-            targets: ["ZeroBoundSharedModels"]),
+        .executable(name: "ZeroBoundSharedModels", targets: ["ZeroBoundSharedModels"])
+    ],
+    dependencies: [
+        // Vapor web framework
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.92.0"),
+    .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+    .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.11.0"),
+    .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.27.0"),
+    .package(url: "https://github.com/apple/swift-crypto.git", from: "3.12.0"),
+    .package(url: "https://github.com/swift-server/swift-webauthn.git", from: "1.0.0-alpha.2"),
+    .package(url: "https://github.com/apple/swift-certificates.git", from: "1.14.0"),
+    .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.6.0"),
+    .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.7.0"),
+    .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ZeroBoundSharedModels"),
+        .executableTarget(
+            name: "ZeroBoundSharedModels",
+            dependencies: [
+                .product(name: "PostgresNIO", package: "postgres-nio"),
+                // Force-resolve Crypto with Extras so PBKDF2 is available to deps:
+                .product(name: "_CryptoExtras", package: "swift-crypto"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "WebAuthn", package: "swift-webauthn"),
+                .product(name: "X509", package: "swift-certificates"),
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession")
+            ],
+            path: "Sources",
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+            ])
     ]
 )
